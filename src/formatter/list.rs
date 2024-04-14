d53d14555bcfaf99949e59da3d00a01207431456
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-14 18:11:53 +0300

    Add `FormatWithArgs` and implement it for `List<T>`

--- src/formatter/list.rs
+++ src/formatter/list.rs
@@ -1,9 +1,18 @@
-use luau_parser::types::List;
+use luau_parser::types::{HasRawValue, List};
 
-use crate::types::Format;
+use crate::types::{Format, FormatWithArgs};
 
-impl<T> Format for List<T> {
-    fn format(&self) -> String {
-        todo!()
+impl<T: Format> FormatWithArgs<&str> for List<T> {
+    fn format_with_args(&self, join_with: &str) -> String {
+        self.items
+            .iter()
+            .map(|item| match item {
+                luau_parser::types::ListItem::Trailing { item, separator } => {
+                    format!("{}{}", item.format(), separator.get_raw_value())
+                }
+                luau_parser::types::ListItem::NonTrailing(item) => item.format(),
+            })
+            .collect::<Vec<String>>()
+            .join(join_with)
     }
 }
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,6 +1,9 @@
 pub trait Format {
     fn format(&self) -> String;
 }
+pub trait FormatWithArgs<P> {
+    fn format_with_args(&self, parameter: P) -> String;
+}
 
 pub trait Fold {
     fn fold(&self, formatted_string: &str) -> String {
