02702a6208ea54aa33ea16a973b8416e37a9eb31
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-23 07:47:00 +0300

    Fix formatting issue for `DoBlock`s

--- src/formatter/blocks/do_block.rs
+++ src/formatter/blocks/do_block.rs
@@ -2,13 +2,14 @@
 
 use luau_parser::types::DoBlock;
 
-use crate::types::Format;
+use crate::{types::Format, TAB};
 
 impl Format for DoBlock {
     fn format(&self, indentation: &mut i32) -> String {
         format!(
-            "do\n{}end",
-            self.body.format(&mut (*indentation + 1))
+            "do\n{}{}end",
+            self.body.format(&mut (*indentation + 1)),
+            TAB.repeat(*indentation as usize),
         )
     }
 }
