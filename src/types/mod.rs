3da70ac97d3cf24e166707a391d64f47fa24a400
msix29 <abdulrehmanessam7567@gmail.com>
2024-05-09 07:27:38 +0300

    Rename `Wrape` trait to `Expand` and change doc comments

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -17,10 +17,12 @@ pub trait FormatWithArgs<P> {
     fn format_with_args(&self, indentation: &mut i32, parameter: P) -> String;
 }
 
-/// A trait which represents that this struct can be wrapped into multiple lines.
-pub trait Wrap {
-    /// Wrap this struct into a multi-line string. If possible.
-    fn wrap(&self, formatted_string: &str) -> String {
+/// A trait which represents that this struct can be expanded into multiple lines.
+pub trait Expand {
+    /// Expand this struct into a multi-line string, if possible. Default implementation
+    /// just returns the string as-is. In most cases, the formatted string isn't
+    /// actually needed nor will be used, but it's passed in case.
+    fn expand(&self, formatted_string: &str) -> String {
         formatted_string.to_string()
     }
 }
