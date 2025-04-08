f813916dcab8204eca0a4125f35b22eef4de1c4a
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-08 23:14:22 +0300

    Update `Format` and `FormatWithArgs` traits

--- src/traits.rs
+++ src/traits.rs
@@ -1,14 +1,16 @@
 //! Module holding all types used by this crate, and optionally by others.
 
+use crate::config::Config;
+
 /// A trait which represents that this struct can be formatted.
 pub trait Format {
     /// Format this struct into a string.
-    fn format(&self, indentation: &mut i32) -> String;
+    fn format(&self, indentation: &mut i32, config: &Config) -> String;
 }
 
 /// A trait which represents that this struct can be formatted, the passed arguments
 /// customize the end result.
 pub trait FormatWithArgs<P> {
     /// Format this struct into a string.
-    fn format_with_args(&self, indentation: &mut i32, parameter: P) -> String;
+    fn format_with_args(&self, indentation: &mut i32, config: &Config, parameter: P) -> String;
 }
