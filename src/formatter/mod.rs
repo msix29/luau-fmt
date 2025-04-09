be2ffa9e531936472606f8d90659fb5bd387274c
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-09 22:04:44 +0300

    Implement formatting for basic types

---
+++ src/formatter/mod.rs
@@ -0,0 +1,61 @@
+use luau_parser::{
+    prelude::{Literal, LuauString, Token, TokenType},
+    types::Pointer,
+};
+
+use crate::{
+    config::Config,
+    traits::{Format, FormatWithArgs, Indentation},
+};
+
+mod block;
+
+impl Format for LuauString {
+    fn format(&self, _indentation: Indentation, config: &Config) -> String {
+        //TODO: Length check
+        config.quote_style.apply(self)
+    }
+}
+
+impl Format for Token {
+    fn format(&self, indentation: Indentation, config: &Config) -> String {
+        match &self.token_type {
+            TokenType::Literal(Literal::String(luau_string)) => {
+                luau_string.format(indentation, config)
+            }
+
+            // `unwrap` itself is safe and should never error as this will only be
+            // be called by the library, which checks for the CST's correctness
+            // before starting any of the formatting.
+            token_type => token_type.try_as_string().unwrap_or_default(),
+        }
+    }
+}
+
+impl<T: Format> Format for Option<T> {
+    fn format(&self, indentation: Indentation, config: &Config) -> String {
+        match self {
+            Some(item) => item.format(indentation, config),
+            None => "".to_string(),
+        }
+    }
+}
+impl<A, T: FormatWithArgs<A>> FormatWithArgs<A> for Option<T> {
+    fn format_with_args(&self, indentation: Indentation, config: &Config, args: A) -> String {
+        match self {
+            Some(item) => item.format_with_args(indentation, config, args),
+            None => "".to_string(),
+        }
+    }
+}
+
+impl<T: Format> Format for Pointer<T> {
+    fn format(&self, indentation: Indentation, config: &Config) -> String {
+        (**self).format(indentation, config)
+    }
+}
+impl<A, T: FormatWithArgs<A>> FormatWithArgs<A> for Pointer<T> {
+    fn format_with_args(&self, indentation: Indentation, config: &Config, args: A) -> String {
+        (**self).format_with_args(indentation, config, args)
+    }
+}
