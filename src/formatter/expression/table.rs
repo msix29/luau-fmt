fe70f6b64a5705a9234c52fcfa7fe19de273f67b
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-10 15:27:10 +0300

    Implement `Format` for all table access-related types

--- src/formatter/expression/table.rs
+++ src/formatter/expression/table.rs
@@ -2,5 +2,46 @@
 //!
 //! * [`TableAccessPrefix`]
 //! * [`TableAccessKey`]
-//! * [`TableAccessKey`]
 //! * [`TableAccess`]
+
+use luau_parser::types::{TableAccess, TableAccessKey, TableAccessPrefix};
+
+use crate::{
+    config::Config,
+    traits::{Format, Indentation},
+};
+
+impl Format for TableAccessPrefix {
+    fn format(&self, indentation: Indentation, config: &Config) -> String {
+        match self {
+            TableAccessPrefix::Name(token) => token.format(indentation, config),
+            TableAccessPrefix::FunctionCall(function_call) => {
+                function_call.format(indentation, config)
+            }
+            TableAccessPrefix::ExpressionWrap(bracketed) => bracketed.format(indentation, config),
+        }
+    }
+}
+
+impl Format for TableAccessKey {
+    fn format(&self, indentation: Indentation, config: &Config) -> String {
+        match self {
+            TableAccessKey::Expression(table_key) => table_key.format(indentation, config),
+            TableAccessKey::Name { name, .. } => {
+                ".".to_string() + &name.format(indentation, config)
+            }
+        }
+    }
+}
+
+impl Format for TableAccess {
+    fn format(&self, indentation: Indentation, config: &Config) -> String {
+        let mut string = self.prefix.format(indentation, config);
+
+        for key in self.accessed_keys.iter() {
+            string.push_str(&key.format(indentation, config));
+        }
+
+        string
+    }
+}
