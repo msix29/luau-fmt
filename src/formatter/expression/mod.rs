0c6c9372abad576c88ffdc6313670c9c80ec963b
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-10 15:37:28 +0300

    Implement `Format` for all expression-related types

--- src/formatter/expression/mod.rs
+++ src/formatter/expression/mod.rs
@@ -26,18 +26,71 @@ impl Format for PrefixExp {
 
 impl Format for Expression {
     fn format(&self, indentation: Indentation, config: &Config) -> String {
-        todo!()
+        match self {
+            Expression::ERROR => unreachable!(),
+            Expression::Nil(token)
+            | Expression::Boolean(token)
+            | Expression::Number(token)
+            | Expression::String(token) => token.format(indentation, config),
+            Expression::Closure(closure) => closure.format(indentation, config),
+            Expression::FunctionCall(function_call) => function_call.format(indentation, config),
+            Expression::ExpressionWrap(bracketed) => {
+                bracketed.format_with_args(indentation, config, " ")
+            }
+            Expression::Var(var) => var.format(indentation, config),
+            Expression::Table(table) => table.format_with_args(indentation, config, false),
+            Expression::UnaryExpression {
+                operator,
+                expression,
+            } => operator.format(indentation, config) + &expression.format(indentation, config),
+            Expression::BinaryExpression {
+                left,
+                operator,
+                right,
+            } => {
+                left.format(indentation, config)
+                    + &operator.format(indentation, config)
+                    + &right.format(indentation, config)
+            }
+            Expression::TypeCast {
+                expression,
+                operator,
+                cast_to,
+            } => {
+                expression.format(indentation, config)
+                    + " :: "
+                    + cast_to.format(indentation, config)
+            }
+            Expression::IfExpression(if_expression) => if_expression.format(indentation, config),
+        }
     }
 }
 
 impl Format for IfExpression {
     fn format(&self, indentation: Indentation, config: &Config) -> String {
-        todo!()
+        let mut string = "if ".to_string();
+        string.push_str(&self.condition.format(indentation, config));
+        string.push_str(" then");
+        string.push_str(&self.if_expression.format(indentation, config));
+
+        for else_if in self.else_if_expressions.iter() {
+            string.push_str(&else_if.format(indentation, config));
+        }
+
+        string.push_str(" else ");
+        string.push_str(&self.else_expression.format(indentation, config));
+
+        string
     }
 }
 
 impl Format for ElseIfExpression {
     fn format(&self, indentation: Indentation, config: &Config) -> String {
-        todo!()
+        let mut string = " elseif ".to_string();
+        string.push_str(&self.condition.format(indentation, config));
+        string.push_str(" then ");
+        string.push_str(&self.expression.format(indentation, config));
+
+        string
     }
 }
