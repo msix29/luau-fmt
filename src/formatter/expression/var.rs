1e0d8b3b741f00c8ee2cc05bc1aa74216b8a5bd7
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-10 15:29:23 +0300

    Implement `Format` for `Var`

--- src/formatter/expression/var.rs
+++ src/formatter/expression/var.rs
@@ -1,1 +1,20 @@
 //! All `impl` blocks for [`Var`].
+
+use luau_parser::types::Var;
+
+use crate::{
+    config::Config,
+    formatter::TokenFormatType,
+    traits::{Format, FormatWithArgs, Indentation},
+};
+
+impl Format for Var {
+    #[inline]
+    fn format(&self, indentation: Indentation, config: &Config) -> String {
+        match self {
+            Var::ERROR => unreachable!(),
+            Var::Name(token) => token.format_with_args(indentation, config, TokenFormatType::Name),
+            Var::TableAccess(table_access) => table_access.format(indentation, config),
+        }
+    }
+}
