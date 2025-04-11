f1337ff427c3fb38592ad149f696265691410379
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-11 13:46:19 +0300

    Fix some code and imports

--- src/formatter/block/while_loop.rs
+++ src/formatter/block/while_loop.rs
@@ -2,11 +2,14 @@
 
 use luau_parser::types::WhileLoop;
 
-use crate::traits::Format;
+use crate::{
+    config::Config,
+    traits::{Format, Indentation},
+};
 
 impl Format for WhileLoop {
-    fn format(&self, indentation: crate::traits::Indentation, config: &crate::config::Config) -> String {
-        let string = "while ".to_string();
+    fn format(&self, indentation: Indentation, config: &Config) -> String {
+        let mut string = "while ".to_string();
         string.push_str(&self.condition.format(indentation, config));
         string.push(' ');
         string.push_str(&self.do_block.format(indentation, config));
