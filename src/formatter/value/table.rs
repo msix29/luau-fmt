f9e3e56e172d125cd90f334dffb872456da78496
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-11 14:17:22 +0300

    Implement `Format` for `Table`

--- src/formatter/value/table.rs
+++ src/formatter/value/table.rs
@@ -5,10 +5,10 @@
 //! * [`TableFieldValue`]
 //! * [`Table`]
 
-use luau_parser::types::{Table, TableField, TableFieldValue, TableKey};
+use luau_parser::types::{Expression, Table, TableField, TableFieldValue, TableKey, TypeValue};
 
 use crate::{
-    config::Config,
+    config::{CompactTable, Config, TrailingCommas},
     formatter::TokenFormatType,
     traits::{Format, FormatWithArgs, Indentation},
 };
@@ -53,6 +53,75 @@ impl Format for TableFieldValue {
 
 impl FormatWithArgs<bool> for Table {
     fn format_with_args(&self, indentation: Indentation, config: &Config, is_type: bool) -> String {
-        todo!()
+        let single_line = match config.compact_table {
+            CompactTable::Always => true,
+            CompactTable::OnlyLiterals => !self.0.iter().all(|item| match &*item.value {
+                // Should we handle wraps?
+                TableFieldValue::Expression(
+                    Expression::Nil(_)
+                    | Expression::Boolean(_)
+                    | Expression::Number(_)
+                    | Expression::String(_),
+                    // Should we include `Expression::Var(_)`?
+                ) => false,
+                TableFieldValue::Type(
+                    TypeValue::String(_) | TypeValue::Boolean(_) | TypeValue::Nil(_),
+                ) => false,
+                _ => true,
+            }),
+            CompactTable::SingleElement => self.0.len() == 1,
+            CompactTable::Never => false,
+        };
+
+        if single_line {
+            let spaces = " ";
+            let separator = ",".to_string() + spaces;
+            let mut string = "{".to_string()
+                + spaces
+                + &self
+                    .0
+                    .format_with_args(indentation, config, (&separator, is_type));
+
+            match config.trailing_commas {
+                TrailingCommas::Always if !string.ends_with(&separator) => {
+                    string.push_str(&separator)
+                }
+                TrailingCommas::Never => {
+                    string = string.trim_end_matches(&separator).to_string() + spaces
+                }
+                _ => {
+                    if !string.ends_with(&separator) {
+                        string.push_str(spaces);
+                    }
+                }
+            }
+
+            string + "}"
+        } else {
+            let spaces = config.newline_style.to_string()
+                + &config.indent_style.to_string(indentation + 1, config);
+            let separator = ",".to_string() + &spaces;
+
+            let mut string = "{".to_string()
+                + &spaces
+                + &self
+                    .0
+                    .format_with_args(indentation, config, (&separator, is_type));
+
+            match config.trailing_commas {
+                TrailingCommas::Never => string = string.trim_end_matches(&separator).to_string(),
+                _ if !string.ends_with(&separator) => string.push_str(&separator),
+                _ => {
+                    if !string.ends_with(&separator) {
+                        string.push_str(&spaces);
+                    }
+                }
+            }
+
+            // Remove the last space.
+            string.pop();
+
+            string + "}"
+        }
     }
 }
