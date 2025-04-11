539eacbf021c548217e8dba2b97986d8ec1e8dea
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-11 13:36:42 +0300

    Implement `Format` for `Bracketed<T>`

--- src/formatter/bracketed.rs
+++ src/formatter/bracketed.rs
@@ -1,1 +1,28 @@
 //! All `impl` blocks for [`Bracketed`].
+
+use luau_parser::types::Bracketed;
+
+use crate::{
+    config::Config,
+    traits::{Format, FormatWithArgs, Indentation},
+};
+
+impl<T: Format> Format for Bracketed<T> {
+    fn format(&self, indentation: Indentation, config: &Config) -> String {
+        let mut string = self.opening_bracket.format(indentation, config);
+        string.push_str(&self.item.format(indentation, config));
+        string.push_str(&self.closing_bracket.format(indentation, config));
+
+        string
+    }
+}
+
+impl<A, T: FormatWithArgs<A>> FormatWithArgs<A> for Bracketed<T> {
+    fn format_with_args(&self, indentation: Indentation, config: &Config, args: A) -> String {
+        let mut string = self.opening_bracket.format(indentation, config);
+        string.push_str(&self.item.format_with_args(indentation, config, args));
+        string.push_str(&self.closing_bracket.format(indentation, config));
+
+        string
+    }
+}
