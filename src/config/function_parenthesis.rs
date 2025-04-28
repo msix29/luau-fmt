f52cd26462a3a1db0bb3e26019df1a15f8804658
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-28 11:49:44 +0300

    Add `FunctionParenthesis::Always` configuration

--- src/config/function_parenthesis.rs
+++ src/config/function_parenthesis.rs
@@ -5,8 +5,11 @@
 #[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord)]
 #[derive(serde::Serialize, serde::Deserialize)]
 pub enum FunctionParenthesis {
-    // Keep it as it is; don't add nor remove parenthesis.
+    // Always include parenthesis.
     #[default]
+    Always,
+
+    // Keep it as it is; don't add nor remove parenthesis.
     Keep,
 
     /// Remove them only for strings (one argument max).
